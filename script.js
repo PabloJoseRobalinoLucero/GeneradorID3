c3095f2bcdaabf5c2822296b723b59f5f7c48f9e
Pablo Jose Robalino Lucero <pablorobalinotrabajo@hotmail.com>
2024-09-16 17:08:17 -0500

    Ejercicio de Clase

---
+++ script.js
@@ -0,0 +1,20 @@
+let cantidad = document.getElementById("cantidad")
+let contrasena = document.getElementById("contrasena")
+
+const cadenaCaracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+function generar() {
+    let numeroDigitado = parseInt(cantidad.value)
+    let password = ""
+
+    if (numeroDigitado < 6) {
+        alert("La contraseña no puede tener menos de 6 caracteres.")
+    } else {
+        for (let i = 0; i < numeroDigitado; i++) {
+            let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)]
+            password += caracterAleatorio
+        }
+    }
+
+    contrasena.value = password
+}
